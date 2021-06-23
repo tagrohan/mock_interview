d58ca5dfcc1a105c37adcbd9736d8935ebb36f21
tagrohan <tagrohan@gmail.com>
2021-06-23 23:48:04 +0530

    Intervie noOfDistinctPairsToSum added

--- src/GFG_random/GFG.java
+++ src/GFG_random/GFG.java
@@ -1,29 +1,53 @@
 package GFG_random;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
 public class GFG {
-    public static void main(String[] args) {
-        for (Integer i : findLeader(new int[] { 16, 17, 4, 3, 5, 2 })) {
-            System.out.print(i + " ");
-        }
-    }
+   public static void main(String[] args) {
+      System.out.println(noOfDistinctPairsToSum(new int[]{1, 2, 1, 2}, 3));
+   }
 
-    // leader is some who is greater to it's right
-    private static List<Integer> findLeader(int[] arr) {
-        int len = arr.length;
-        Stack<Integer> stack = new Stack<>();
-        List<Integer> list = new ArrayList<>();
-        for (int i = len - 1; i >= 0; i--) {
-            while (!stack.isEmpty() && stack.peek() <= arr[i])
-                stack.pop();
-            if (stack.isEmpty())
-                list.add(arr[i]);
-            stack.push(arr[i]);
-        }
-        return list;
-    }
+   private static int noOfDistinctPairsToSum(int[] arr, int sum) {
+//      System.out.println(noOfDistinctPairsToSum(new int[]{1, 2, 1, 2}, 3));
+      Arrays.sort(arr);
+      int count = 0;
+      int low = 0, high = arr.length - 1;
+      while (low < high) {
+         if (arr[low] + arr[high] > sum) {
+            high -= 1;
+         } else if (arr[low] + arr[high] < sum) {
+            low += 1;
+         } else {
+            count += 1;
+            low += 1;
+            high -= 1;
+         }
+      }
+      return count;
+   }
+
+
+   // leader is some who is greater to it's right
+   private static List<Integer> findLeader(int[] arr) {
+
+//        for (Integer i : findLeader(new int[] { 16, 17, 4, 3, 5, 2 })) {
+//            System.out.print(i + " ");
+//        }
+
+      int len = arr.length;
+      Stack<Integer> stack = new Stack<>();
+      List<Integer> list = new ArrayList<>();
+      for (int i = len - 1; i >= 0; i--) {
+         while (!stack.isEmpty() && stack.peek() <= arr[i])
+            stack.pop();
+         if (stack.isEmpty())
+            list.add(arr[i]);
+         stack.push(arr[i]);
+      }
+      return list;
+   }
 
 }

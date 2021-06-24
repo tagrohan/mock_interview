e2166c02fa04075f297256dc070db0d67100f7f2
tagrohan <tagrohan@gmail.com>
2021-06-24 23:34:22 +0530

    GFG noOfPairToCertainConditionV2 added

--- src/GFG_random/GFG.java
+++ src/GFG_random/GFG.java
@@ -4,24 +4,59 @@ import java.util.*;
 
 public class GFG {
    public static void main(String[] args) {
-      System.out.println(noOfDistinctPairsToSumV2(new int[]{1, 5, 7, -1}, 6));
+      System.out.println(noOfPairToCertainConditionV2(new int[]{199, 100, 200, 400, 300, 500, 600, 200}));
+
+   }
+
+   private static int noOfPairToCertainConditionV2(int[] arr) {
+//      System.out.println(noOfPairToCertainConditionV2(new int[]{199, 100, 200, 400, 300, 500, 600, 200}));
+      Map<Integer, Integer> map = new HashMap<>();
+      int count = 0;
+      for (int i = 0; i < arr.length; i++) {
+         int val = arr[i] % 200;
+         if (map.containsKey(val)) {
+            count += map.get(val);
+            map.put(val, map.get(val) + 1);
+         } else {
+            map.put(val, 1);
+         }
+      }
+      return count;
+   }
+
+
+   // condition = A[i] - A[j] is multiple of 200 (look like pair sum)
+   private static int noOfPairToCertainCondition(int[] arr) {
+      Map<Integer, Integer> map = new HashMap<>();
+      int count = 0;
+      for (int i = 0; i < arr.length - 1; i++) {
+         for (int j = i + 1; j < arr.length; j++) {
+            if ((arr[i] - arr[j]) % 200 == 0) {
+               map.put(i + 1, j + 1);
+               count += 1;
+            }
+         }
+      }
+      for (int i : map.keySet()) {
+         System.out.println(i + " " + map.get(i));
+      }
+      return count;
    }
 
 
    // using hashset in single traversal works fine here
-   private static int noOfDistinctPairsToSumV2(int[] arr, int sum) {
+   private static int noOfDistinctPairsToSumV2(int[] arr, int sum) { // work on most cases bit its good to go for V3 approach
 //      System.out.println(noOfDistinctPairsToSumV2(new int[]{1, 5, 7, -1}, 6));
       Map<Integer, Integer> map = new HashMap<>();
       int count = 0;
       for (Integer i : arr) {
-         if (map.containsKey(i) || map.containsKey(sum - i)) {
+         if (map.containsKey(sum - i)) {
             if (map.get(sum - i) > 0) {
                count += 1;
                map.put(sum - i, map.get(sum - i) - 1);
-            }
-         } else {
-            map.put(i, 1);
-         }
+            } else map.put(sum - i, 1);
+         } else if (map.containsKey(i)) map.put(i, map.get(i) + 1);
+         else map.put(i, 1);
       }
       return count;
    }
---
+++ src/kenny/DFS.java
@@ -0,0 +1,23 @@
+package kenny;
+
+import java.util.List;
+
+public class DFS {
+   public static void main(String[] args) {
+
+   }
+
+   static String[] arr = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+   private static List<String> phoneCombination(String number) {
+//      String first = arr[number.charAt(0) - '0'];
+//
+//      for (int i = 0; i < first.length(); i++) {
+//         char ch = first.charAt(i);
+//         List<String> res = phoneCombination(number.substring(1));
+//
+//      }
+      return List.of("");
+   }
+
+}
